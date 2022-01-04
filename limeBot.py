fdeb0df24e8e99ae5200d7fbd1c7a5ecdba84be0
ma5081 <45620419+ma5081@users.noreply.github.com>
2022-01-05 01:25:00 +0400

    uploaded code

---
+++ limeBot.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+from discord.utils import get
+
+
+outfile = open ("logs.txt", 'a')
+
+intents = discord.Intents.default()
+intents.guilds = True
+client = commands.Bot(command_prefix = ';')
+
+@client.event
+async def on_ready():
+    client.guilds
+
+@client.command()
+async def a(ctx, *, msg=""):
+    await ctx.message.delete()
+    if msg !="":
+        await ctx.send(f"asking for a friend: {msg}")
+    else:
+        await ctx.send("to send an anonymous message, use ```;a <message>```")
+
+@client.command()
+async def t(ctx, ser: discord.Guild, chann="",  *, msg=""):
+    if chann == "":
+        await ctx.send(f"to send an anonymous message, use ```;t {serv} <channel_name> <message>```")
+        return
+    for chan in ser.text_channels:
+        if chan.name == chann:
+            if msg !="":
+                if chan.permissions_for(ctx.author).send_messages:
+                    outfile.write(f"{ctx.author} in {chann}: {msg}\n")
+                    await chan.send(f"asking for a friend: {msg}")
+                    await ctx.send("sent! stay bitter!")
+                else:
+                    await ctx.send("I can't send that >:(")
+                return
+            else:
+                await ctx.send(f"to send an anonymous message, use ```;t {serv} {chann} <message>```")
+                return
+    await ctx.send(f"to send an anonymous message, use ```;t {serv} <channel_name> <message>```make sure the channel name is written correctly (and exists in the given server)")
+    return
+client.run("NzczNjMyOTkxMTc5MDQ2OTIy.X6MD3g.4c08MRNVlNDY4Oj7yFfGe-bcuLA")
+outfile.close()
